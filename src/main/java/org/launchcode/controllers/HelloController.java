4ed862386deb586aae3c4778fdb2e75b0acd6c9b
Elizabeth Niebauer <pudgykogima@Elizabeths-MacBook-Pro.local>
2017-05-01 11:12:23 -0500

    video-controllers-end

---
+++ src/main/java/org/launchcode/controllers/HelloController.java
@@ -0,0 +1,25 @@
+package org.launchcode.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by LaunchCode
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping(value = "")
+    @ResponseBody
+    public String index() {
+        return "Hello World";
+    }
+
+    @RequestMapping(value = "goodbye")
+    @ResponseBody
+    public String goodbye() {
+        return "Goodbye";
+    }
+
+}
